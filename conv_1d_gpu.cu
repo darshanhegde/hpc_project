64343dd99b564d1bc8c98cf02f1918ce26695b41
darshanhegde <darshan.hegde@cims.nyu.edu>
2015-05-18 00:07:05 -0400

    Syncing with master

--- conv_1d_gpu.cu
+++ conv_1d_gpu.cu
@@ -184,7 +184,6 @@ void conv1d_kernel(WORDVECS wordvec, KERNS kerns, OUTPUTS output){
                 s[tIdx] += (wv[j*dim+tIdx] * kerns.k[k*kerns.width*kerns.height + k_sub*kerns.height + tIdx]);
             }
             atomicAdd(&out[i*kerns.num+k], 1);
-            out[i*kerns.num+k] = 1.;
             __syncthreads();
         }
     }
